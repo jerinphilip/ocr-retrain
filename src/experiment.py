03f04f609890a3001a7bc69dcc407d32823f55b8
Jerin Philip <jerin.philip@students.iiit.ac.in>
2017-05-24 19:06:59 +0530

    Changes done; Code pushed for the day; Analysis of output left;

--- src/experiment.py
+++ src/experiment.py
@@ -1,46 +1,35 @@
-
-#!/usr/bin/python3
-
-#SBATCH --output=output/parallel-%j.out
-#SBATCH --partition=atom-cpu
-#SBATCH -n 16 # 10 cores
-#SBATCH -N 7
-#SBATCH --overcommit
-
-import sys
 import os
-import multiprocessing
-
-# Necessary to add cwd to path when script run
-# by SLURM (since it executes a copy)
-sys.path.append(os.getcwd())
-
-
 from ocr import GravesOCR
 import numpy as np
 from error_module import Dictionary
 import cv2
 import sys
 from aux.tokenizer import tokenize
-from parser import webtotrain
+import parser.webtotrain as webtotrain
 from timeit import timeit
 from Levenshtein import distance
 from random import randint
 import json
 from pprint import pprint
 from functools import partial
+from math import floor,ceil
 
-def split(data, fraction):
-    assert(0 <= fraction && fraction <= 1)
+def split_index(data, fraction):
+    assert(0 <= fraction and fraction <= 1)
     total = len(data)
     num_first = floor(fraction*total)
     num_second = total - num_first
-    first = data[:num_first]
-    second = data[num_first:]
-    return (first, second)
+    print("Num First, Num Second: ", num_first, num_second)
+    #print(data[:num_first])
+    split_index = 0
+    for i in range(num_first):
+        split_index += len(data[i][0])
+    print("Split Index:", split_index)
+    return split_index
 
 def stats(ocr, em, book_path):
     pagewise = webtotrain.read_book(book_path)
+    #pagewise = pagewise[5:10]
     images, truths = [], []
     for imgs, ts in pagewise:
         images.extend(imgs)
@@ -52,85 +41,53 @@ def stats(ocr, em, book_path):
     stat_d = {}
 
     for fraction in [0.2, 0.4, 0.6, 0.8]:
-        first, second = split(pagewise, fraction)
-        vocab_from_book = []
-        for imgs, ts in first:
-            vocab_from_book.extend(ts)
-        em.enhance_vocabulary(vocab_from_book)
-        print("Computing Errors", flush=True)
+        si = split_index(pagewise, fraction)
+        em.enhance_vocabulary(truths[:si])
+        print("Computing Errors, with fraction %.1f"%(fraction), flush=True)
         errors = [em.error(prediction) for prediction in predictions]
-        tuples = list(zip(truths, predictions, errors))
 
-        threshold_f = lambda x: x[2] == 0
-        correct = filter(threshold_f, tuples)
-        wrong = filter(lambda x: not threshold_f(x), tuples)
+        initial_dict = lambda : {
+                "real_word_error": 0,
+                "correctable": 0,
+                "correct": 0,
+                "uncorrectable":0,
+                "total": 0
+            }
+
 
         sfd = {
-                "real_word_error": 0,
-                "correct": 0,
-                "correctable": 0,
-                "incorrectable": 0
+            "included": initial_dict(),
+            "unseen": initial_dict()
         }
-
-        for truth, prediction, error in correct:
-            if truth != prediction:
-                sfd["real_word_error"] += 1
+        threshold = 1
+        for i in range(len(truths)):
+            truth, prediction, error = truths[i], predictions[i], errors[i]
+            dkey = lambda dtype: "included" if dtype else "unseen"
+            sfd[dkey(i<si)]["total"] += 1
+            if error < threshold:
+                if truth != prediction:
+                    sfd[dkey(i<si)]["real_word_error"] += 1
+                else:
+                    sfd[dkey(i<si)]["correct"] += 1
             else:
-                sfd["correct"] += 1
+                suggestions = em.suggest(prediction)
+                if truth in suggestions:
+                    sfd[dkey(i<si)]["correctable"] += 1
+                else:
+                    sfd[dkey(i<si)]["uncorrectable"] += 1
 
-        for truth, prediction, error in wrong:
-            suggestions = em.suggest(prediction)
-            if truth in suggestions:
-                sfd["correctable"] += 1
-            else:
-                sfd["incorrectable"] += 1
-
-        correct = sum([1 for truth,prediction in zip(truths, predictions) \
-                if truth==prediction])
-
-        sfd["word_accuracy"] = (correct/len(images))*100
-        sum_edit_distances = sum(
-                [ distance(truth, prediction) \
-                        for truth, prediction in zip(truths, predictions)]
-                )
-
-        total_length = sum(map(len, truths))
-        sfd["character_error_rate"] = (sum_edit_distances/total_length)*100
-        sfd["correct_without_dict"] = sum(
-                [1 for truth, prediction in zip(truths, predictions)
-                        if truth == prediction]
-                )
-        sfd["total"] = len(images)
         stat_d[fraction] = sfd
 
     stat_d["book_dir"] = book_path
     return stat_d
 
-def work(config, book_loc):
-    ocr = GravesOCR(config["model"], config["lookup"])
-    error = Dictionary(**config["error"])
-    statd = stats(ocr, error, book_loc)
-    return statd
-
-
 if __name__ == '__main__':
     config = json.load(open(sys.argv[1]))
-
-    ncpus = None
-    # get number of cpus available to job
-    try:
-        ncpus = int(os.environ["SLURM_JOB_CPUS_PER_NODE"])
-    except KeyError:
-        ncpus = multiprocessing.cpu_count()
-
-    print("ncpus found:", ncpus)
-    partial_work = partial(work, config)
-    book_locs = map(lambda x: config["dir"] + x + '/', config["books"])
-    p = multiprocessing.Pool(ncpus)
-    stats_ls = p.map(partial_work, book_locs)
-    p.close()
-    p.join()
-    print(list(stats_ls))
-
-
-
+    book_index = int(sys.argv[2])
+    output_dir = 'output'
+    ocr = GravesOCR(config["model"], config["lookup"])
+    error = Dictionary(**config["error"])
+    book_locs = list(map(lambda x: config["dir"] + x + '/', config["books"]))
+    stat_d = stats(ocr, error, book_locs[book_index])
+    with open('%s/ml-ocr/stats_%s.json'%(output_dir, config["books"][book_index]), 'w+') as fp:
+        json.dump(stat_d, fp, indent=4)
