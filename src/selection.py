76838d166a8c54d85220d3331a99eac958b36e93
Jerin Philip <jerin.philip@students.iiit.ac.in>
2017-06-26 10:24:55 +0530

    Let's see how this one goes

---
+++ src/selection.py
@@ -0,0 +1,18 @@
+import heapq
+from random import randint
+
+def pick_best(**kwargs):
+    k = kwargs['count']
+    key = kwargs['key']
+    def best_k_key(d):
+        return heapq.nlargest(k, d.items(), key=key)
+
+
+def sequential(d_item):
+    index, rest = d_item
+    return index
+
+def random_index(d_item):
+    return randint(10000)
+
+
