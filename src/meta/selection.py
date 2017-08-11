056eef8b7b7b62cdcf545457a2ec1f348c2b10ad
Deepayan <deepayan137@gmail.com>
2017-08-11 14:24:34 +0530

    changes

--- src/meta/selection.py
+++ src/meta/selection.py
@@ -37,33 +37,35 @@ def random_index(d_item, **kwargs):
 def word_freqency(d_item, **kwargs):
 	try:
 		indices, predictions = zip(*d_item)
+
+		indices, predictions = list(indices), list(predictions)
+		print (len(predictions))
+		print(predictions[:10])
+		k = kwargs['count']
+		most_common_words = pick_most_common(predictions,k)
+
+		#print (most_common_words)
+		k_best = [predictions.index(each_word) for each_word in most_common_words]
+		return k_best
 	except ValueError as e:
 		print('Not enough argument')
-	indices, predictions = list(indices), list(predictions)
-	print (len(predictions))
-	print(predictions[:10])
-	k = kwargs['count']
-	most_common_words = pick_most_common(predictions,k)
-
-	#print (most_common_words)
-	k_best = [predictions.index(each_word) for each_word in most_common_words]
-	return k_best
 
 
 def word_frequency_v02(d_item, **kwargs):
 	k = kwargs['count']
 	k_best = []
-	indices, predictions = zip(*d_item)
-	indices, predictions = list(indices), list(predictions)
-	rev_d_item = dict(zip(predictions, indices))
-	
-	most_common_words = pick_most_common(predictions,k)
-	#k_best = [rev_d_item[each_word] for each_word in most_common_words](
-	def subset(pos):
-		sub = [indices[p] for p in pos]
-		return sub
-	for word in most_common_words:
-		pos = [i for i,v in enumerate(predictions) if v == word]
-		k_best.extend(subset(pos))
-	
-	return(k_best)
+	try:
+		indices, predictions = zip(*d_item)
+		indices, predictions = list(indices), list(predictions)
+		rev_d_item = dict(zip(predictions, indices))
+		most_common_words = pick_most_common(predictions,k)
+		#k_best = [rev_d_item[each_word] for each_word in most_common_words](
+		def subset(pos):
+			sub = [indices[p] for p in pos]
+			return sub
+		for word in most_common_words:
+			pos = [i for i,v in enumerate(predictions) if v == word]
+			k_best.extend(subset(pos))
+		return(k_best)
+	except ValueError as e:
+		print('Not enough argument')
