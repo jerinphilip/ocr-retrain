863b6d30f7e109f6808241276ceb195330fd1588
Deepayan137 <deepayan137@gmail.com>
2017-08-12 14:09:48 +0530

    driver code

---
+++ src/run_ocr.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#SBATCH --job-name=hi-ocr    # Job name
+#SBATCH --time=INFINITE               # Time limit hrs:min:sec
+#SBATCH --partition=atom-cpu
+#SBATCH --output=outputs/out/%j.out
+#SBATCH --array=0-31
+#SBATCH --mem=4G
+#SBATCH -n 1
+#SBATCH --error=outputs/out/%j.err
+#SBATCH --overcommit
+#python3 retrain.py configs/Hindi.json $SLURM_ARRAY_TASK_ID Hindi
+python3 tests/test.py configs/malayalam.json  $SLURM_ARRAY_TASK_ID ml
