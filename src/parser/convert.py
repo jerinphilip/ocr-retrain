25ac01dc9c0c915f9c7569bd7d84203165f3bb88
Jerin Philip <jerin.philip@students.iiit.ac.in>
2017-08-12 15:19:06 +0530

    Making changes to convert

--- src/parser/convert.py
+++ src/parser/convert.py
@@ -4,7 +4,7 @@
 def page_to_unit(pagewise):
     images, truths = [], []
     for im, tr in pagewise:
-        images.extends(im)
-        truths.extends(tr)
+        images.extend(im)
+        truths.extend(tr)
     #units = tuple(zip(images, truths)) 
     return (images,truths) #(list of images, list of truths)
