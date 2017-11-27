a77ca666c846b2622502ada0f4e6bd3514e05a8a
Deepayan <deepayan137@gmail.com>
2017-11-27 21:09:41 +0530

    revised code for clustering

--- doctools/scripts/cluster.py
+++ doctools/scripts/cluster.py
@@ -1,5 +1,5 @@
 from doctools.cluster.mst import cluster
-from doctools.cluster.distance import jaccard, lev
+from doctools.cluster.distance import jaccard, lev, euc, cos
 from doctools.parser.convert import page_to_unit
 from doctools.parser import webtotrain
 from argparse import ArgumentParser
@@ -8,12 +8,89 @@ from .dot import as_dot
 from distance import levenshtein
 import json
 from functools import partial
+import pdb
 from doctools.ocr import GravesOCR
 import os
 from .opts import base_opts
+import numpy as np
+import matplotlib
+matplotlib.use('agg')
+from matplotlib import pyplot as plt
+import cv2
+import pdb
+from sklearn.neighbors import KNeighborsClassifier
+from collections import Counter
+
+def get_index(a):
+    uniques = np.unique(np.array(a))
+    idx = {u:[] for u in uniques}
+    for i, v in enumerate(a):
+        idx[v].append(i)
+    return dict(sorted(idx.items(),key=lambda x: -len(x[1])))
+
+def visualize(path, components):
+    with open(os.path.join(fpath, 'annotation.txt'), 'r') as in_file:
+        lines = in_file.readlines()
+    words = [line.rsplit()[1] for line in lines]
+    number_of_subplots = 10
+    images = [line.rsplit()[0] for line in lines]
+    for i,v in enumerate(components):
+        print("cluster no: %d"%i)
+        number_of_subplots = min(len(v), number_of_subplots)
+        for j in range(number_of_subplots):
+            
+            print(os.path.join(path,images[v[j]]))
+            im = cv2.imread(os.path.join(path,images[v[j]]))
+            print("subploting [%d/%d]"%(j,len(v)))
+            print(1,number_of_subplots, j+1)
+            plt.subplot(1,number_of_subplots, j+1),plt.xticks([]), plt.yticks([])
+            plt.imshow(im)
+        plt.savefig(os.path.join(args.output,'cluster_%d.png'%i))
+    # plt.show() 
+
+
+def k_nearest(fpath):
+    words=[]
+    with open(os.path.join(fpath, 'annotation.txt'), 'r') as in_file:
+        lines = in_file.readlines()
+    words = [line.rsplit()[1] for line in lines]
+    test_words = np.array(words[2000:])
+    test_features = np.array(features[2000:])
+    test_index = get_index(test_words)
+    indices = sum([v  for i,v in test_index.items() if len(v)>=3],[])
+    test_words = test_words[indices]
+    test_features = test_features[indices]
+    neigh.fit(features[:2000], words[:2000])
+    
+    y_predict = neigh.predict(test_features)
+    pdb.set_trace()
+    acc = [1 if y_predict[i]==test_words[i] else 0 for i in  range(len(test_words))]
+    print(sum(acc)/len(test_words))
+
+def get_predictions(fpath):
+    print("Reading book...", end='', flush=True)
+    pagewise = webtotrain.read_book(fpath)
+    print("Done")
+    images, truths = page_to_unit(pagewise)
+    print("Predicting....", end='', flush=True)
+    predictions = ocr.predict(images)
+    print("Done")
+    errored = [predictions[i] for i in range(len(truths)) if predictions[i] != truths[i]]
+    return errored
+
+def get_features(fpath):
+    feat = np.load(os.path.join(fpath, "feats.npy"))
+    features = [feat[i] for i in range(feat.shape[0])]
+    return features
+
+def form_clusters(elements, dist):
+    print("Clustering....", end='', flush=True)
+    edges, components = cluster(elements, dist, threshold=0.5, prune_above=0.8, rep='components')
+    print("Done")
+    return edges, components
 
 if __name__ == '__main__':
-    lev2 = partial(levenshtein, normalized=True)
+ 
     parser = ArgumentParser()
     base_opts(parser)
     args = parser.parse_args()
@@ -21,34 +98,25 @@ if __name__ == '__main__':
     config = json.load(config_file)
 
     # Load OCR
+    print(config["model"])
     ocr = GravesOCR(config["model"], config["lookup"])
 
     # Parse Book in and predict
     book_name = config["books"][args.book]
+    feat_path = os.path.join(config["feat_dir"], config["books"][args.book])
     fpath = os.path.join(config["dir"], config["books"][args.book])
-
-    print("Reading book...", end='', flush=True)
-    pagewise = webtotrain.read_book(fpath)
-    #pagewise = pagewise[5:7]
-    print("Done")
-
-    images, truths = page_to_unit(pagewise)
-
-    print("Predicting....", end='', flush=True)
-    predictions = ocr.predict(images)
-    print("Done")
-
-    # Cluster
-
-    errored = [predictions[i] for i in range(len(truths)) if predictions[i] != truths[i]]
-    print("Clustering....", end='', flush=True)
-    edges, components = cluster(errored, lev2, threshold=0.5, prune_above=0.8, rep='components')
-    print("Done")
-
-    for c, component in enumerate(components):
+    # # neigh = KNeighborsClassifier(n_neighbors=3)
+    features = get_features(feat_path)
+    errors = get_predictions(fpath)
+    edges_word, comp_words = form_clusters(errors, lev)
+    edges_feat, comp_feat = form_clusters(features, cos)
+    for c, component in enumerate(comp_words):
         #rep = as_dot(predictions, edges, component) 
-        rep = as_dot(errored, edges, component) 
+        rep = as_dot(errors, edges_word, comp_word) 
         of = os.path.join(args.output, "{}.dot".format(c))
         #of = "{}_{}.dot".format(args.output, c)
         with open(of, "w+") as ofp:
             ofp.write(rep)
+    
+    # visualize(fpath, components)
+    
