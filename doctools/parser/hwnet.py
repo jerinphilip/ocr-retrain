28045160eda893ae95daebcbc880618c14e746b5
Jerin Philip <jerin.philip@students.iiit.ac.in>
2017-12-05 13:59:27 +0530

    Image clustering needs refinement

--- doctools/parser/hwnet.py
+++ doctools/parser/hwnet.py
@@ -1,5 +1,16 @@
 
 def read_annotation(fpath):
+    """
+    Reads annotation.txt by praveen krishnan
+    returns (basepath, imgs, truths)
+    """
+    base = fpath
+    imgs = []
+    truths = []
     with open(fpath) as fp:
         for line in fp:
-            print(line.strip())
+            img, truth = line.strip().split()
+            imgs.append(img)
+            truths.append(truth)
+
+    return (base, imgs, truths)
