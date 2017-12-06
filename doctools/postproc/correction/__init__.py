e2220b16a66397bf85602364ac2e003c36f4999d
Deepayan <deepayan137@gmail.com>
2017-12-06 21:51:03 +0530

    correction init

--- doctools/postproc/correction/__init__.py
+++ doctools/postproc/correction/__init__.py
@@ -1,7 +1,4 @@
 from .params import params
-<<<<<<< HEAD
-from .base import naive, suggest
+from .base import naive, suggest, cluster
 from doctools.scripts.opts import base_opts
-=======
-from base import naive, suggest, cluster
->>>>>>> 801c9060684f4c0e64f8afeb40c6aae04a0cd296
+
