dd4aa34fd122887ebefd47223d710c06c2f18c36
Jerin Philip <jerin.philip@students.iiit.ac.in>
2017-12-08 16:42:06 +0530

    Refactoring save

--- doctools/scripts/cluster.py
+++ doctools/scripts/cluster.py
@@ -1,13 +1,11 @@
 from doctools.cluster.mst import cluster, merge
-from doctools.cluster.distance import jaccard, lev, euc, cos
+from doctools.cluster.distance import jaccard, lev, euc, cos, norm_euc
 from doctools.parser.convert import page_to_unit
 from doctools.meta.file_locs import get_pickeled, get_clusters
 from doctools.cluster.k_nearest.distance import normalized_euclid_norm
 from doctools.parser import read_book
 from argparse import ArgumentParser
 from pprint import pprint
-# from .dot import as_dot
-# from distance import levenshtein
 import json
 from functools import partial
 import pdb
@@ -15,77 +13,27 @@ from doctools.ocr import GravesOCR
 import os
 from .opts import base_opts
 import numpy as np
-import matplotlib
-matplotlib.use('agg')
-from matplotlib import pyplot as plt
 import cv2
 import pdb
 from sklearn.neighbors import KNeighborsClassifier
 from collections import Counter
 from .debug import time
 import pickle
-def get_index(a):
-    uniques = np.unique(np.array(a))
-    idx = {u:[] for u in uniques}
-    for i, v in enumerate(a):
-        idx[v].append(i)
-    return dict(sorted(idx.items(),key=lambda x: -len(x[1])))
+
+import matplotlib
+matplotlib.use('agg')
+from matplotlib import pyplot as plt
+
 def save(**kwargs):
     data = kwargs['data']
     meta = kwargs['book']
     loc = kwargs['outpath']
-    if kwargs['feat'] == "predictions":
-        with open('%s/%s.pkl'%(loc, meta),'wb') as f:
-            pickle.dump(data, f)
-        print('Book predictions saved..')
-    if kwargs['feat'] == "images":
-        with open('%s/%s.features_cluster.pkl'%(loc, meta),'wb') as f:
-            pickle.dump(data, f)
-        print('image featutres saved..')
-    if kwargs['feat'] == "words":
-        with open('%s/%s.words_cluster.pkl'%(loc, meta),'wb') as f:
-            pickle.dump(data, f)
-        print('words featutres saved..')    
+    fname = "%s.%s.pkl"%(meta, kwargs["feat"])
+    fpath = os.path.join(loc, fname)
+    with open(fpath, "wb") as f:
+        pickle.dump(data, f)
+        print("Saving:", kwargs["feat"])
 
-def visualize(path, components):
-    with open(os.path.join(fpath, 'annotation.txt'), 'r') as in_file:
-        lines = in_file.readlines()
-    words = [line.rsplit()[1] for line in lines]
-    number_of_subplots = 10
-    images = [line.rsplit()[0] for line in lines]
-    for i,v in enumerate(components):
-        print("cluster no: %d"%i)
-        number_of_subplots = min(len(v), number_of_subplots)
-        for j in range(number_of_subplots):
-            
-            print(os.path.join(path,images[v[j]]))
-            im = cv2.imread(os.path.join(path,images[v[j]]))
-            print("subploting [%d/%d]"%(j,len(v)))
-            print(1,number_of_subplots, j+1)
-            plt.subplot(1,number_of_subplots, j+1),plt.xticks([]), plt.yticks([])
-            plt.imshow(im)
-        plt.savefig(os.path.join(args.output,'cluster_%d.png'%i))
-    # plt.show() 
-
-
-def k_nearest(fpath):
-    words=[]
-    with open(os.path.join(fpath, 'annotation.txt'), 'r') as in_file:
-        lines = in_file.readlines()
-    words = [line.rsplit()[1] for line in lines]
-    neigh = KNeighborsClassifier(n_neighbors=3) 
-    test_words = np.array(words[2000:])
-    test_features = np.array(features[2000:])
-    test_index = get_index(test_words)
-    indices = sum([v  for i,v in test_index.items() if len(v)>=3],[])
-    test_words = test_words[indices]
-    test_features = test_features[indices]
-    neigh.fit(features[:2000], words[:2000])
-    
-    y_predict = neigh.predict(test_features)
-    pdb.set_trace()
-    acc = [1 if y_predict[i]==test_words[i] else 0 for i in  range(len(test_words))]
-    print(sum(acc)/len(test_words))
 @time
 def get_predictions(fpath):
     print("Reading book...", end='', flush=True)
@@ -105,6 +53,7 @@ def get_images(fpath):
     print("Done")
     images, truths = page_to_unit(pagewise)
     return images, truths
+
 @time
 def get_features(fpath):
     print('Loading features...')
@@ -112,6 +61,7 @@ def get_features(fpath):
     features = [feat[i] for i in range(feat.shape[0])]
     print('Done....')
     return features
+
 @time
 def form_clusters(elements, **kwargs):
     dist  = kwargs["distance"]
@@ -120,19 +70,6 @@ def form_clusters(elements, **kwargs):
     edges, components = cluster(elements, dist, threshold=threshold,prune_above=0.8, rep='components')
     print("Done")
     return edges, components
-@time
-def find_examplars(all_edges):
-    return set([ex for ex, c in all_edges])
-@time
-def group_components(exemplars, all_edges):
-    all_comp = []
-    for e in exemplars:
-        comp = []
-        for ex, cl in all_edges:
-            if ex == e:
-                comp.append(cl)
-        all_comp.append(comp)
-    return all_comp
 
 
 if __name__ == '__main__':
