12b48f00c7c65d7125620b3a0b709a46dafe4ede
Jerin Philip <jerin.philip@students.iiit.ac.in>
2017-12-14 23:19:37 +0530

    Fixing levenshtein to normalized

--- doctools/cluster/distance/__init__.py
+++ doctools/cluster/distance/__init__.py
@@ -1,4 +1,8 @@
 from .jaccard import jaccard, inv_jaccard
-from Levenshtein import distance as lev
+from distance import levenshtein
 from scipy.spatial.distance import euclidean as euc
 from scipy.spatial.distance import cosine as cos
+
+from functools import partial
+
+lev = partial(levenshtein, normalized=True)
