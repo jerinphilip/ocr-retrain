39567ee07fe50a31ebd31c2b7d85543a1419efa5
Deepayan <deepayan137@gmail.com>
2018-01-17 00:18:40 +0530

    eng json and slurm scripts

--- slurm-scripts/connect.sh
+++ slurm-scripts/connect.sh
@@ -2,16 +2,17 @@
 #SBATCH --job-name=ml-ocr    # Job name
 #SBATCH --time=3:00:00               # Time limit hrs:min:sec
 #SBATCH --partition=cosmos-debug
-#SBATCH --output=log/%j.out
-#SBATCH --array=0-31
+#SBATCH --output=doctools/outputs/%j.out
+#SBATCH --array=0-6
 #SBATCH --gres=gpu:1
 #SBATCH --mem=8G
 #SBATCH --qos=bogan
 #SBATCH -n 1
+#SBATCH --error=doctools/outputs/%j.err
 #SBATCH --overcommit
 export LANG='en_US.UTF-8'
 python doctools/hwnet/codes/deploy/hw.py \
-    -c doctools/configs/Hindi.json -l hi -b $SLURM_ARRAY_TASK_ID -o /OCRData2/praveen-intermediate/
+    -c doctools/configs/English.json -l en -b $SLURM_ARRAY_TASK_ID -o /OCRData2/deepayan/
     #--img_folder /OCRData2/minesh.mathew/Books/books_postcleaning/Malayalam/0006/0006_101SanjayanPhalithangal_Img_600_Original \
     #--test_vocab_file sample.txt \
     #--pretrained_path /users/jerin/ocr-retrain/doctools/hwnet/pretrained_models/ckpt_best.t7 \
