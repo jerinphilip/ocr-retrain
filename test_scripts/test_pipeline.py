bdbfc81927006a37e4cd09c91b5ab29a734ac2db
Deepayan <deepayan137@gmail.com>
2018-03-04 11:43:17 +0530

    fixed some minor bugs

--- test_scripts/test_pipeline.py
+++ test_scripts/test_pipeline.py
@@ -1,49 +1,52 @@
 import sys
 import torch
+import pdb
 
-root = '/OCRData2/ocr/gpu/'
-sys.path.insert(0, root)
+# root = '/OCRData2/ocr/gpu/'
+# sys.path.insert(0, root)
 
 
 # Test 1 - Parser
-from parser import read_book
-book = '/OCRData2/minesh.mathew/Books/books_postcleaning/Malayalam/0002/'
-pagewise = read_book(book, 'line')
-
+from parser.loader import read_book
+# book = '/OCRData2/minesh.mathew/Books/books_postcleaning/Malayalam/0002/'
+book = 'Advaita_Deepika/'
+pagewise = read_book(book_path=book)
+print('finished reading data')
 # Test 2 - DataLoader
 from ocr.preproc import DataLoader 
-loader = DataLoader(pagewise=pagewise)
+print('Reading Data')
+loader = DataLoader(pagewise=pagewise[2:4])
 
 print("Data split:",)
-print(list(map(len, [loader.split.train, loader.split.val, loader.split.test])))
+# print(list(map(len, [loader.split.train, loader.split.val, loader.split.test])))
 
 
 # Test-3 - Analyze dimensions
 seq, target = loader.split.train[0]
-print(seq.shape)
 
 
 # Test-4 - Test concat function
 from ocr.util import concat
 seq_targ_1, seq_targ_2 = loader.split.train[:2]
-print(concat(seq_targ_1, seq_targ_2))
+# print(concat(seq_targ_1, seq_targ_2))
 
 # Test-5 - Reduce using concat, test out
 width = 2**15
 from ocr.util import knot
-train = knot(loader.split.train, max_width=width)
-validation = knot(loader.split.val, max_width=width)
+# train = knot(loader.split.train, max_width=width)
+# validation = knot(loader.split.test, max_width=width)
 train = loader.split.train
-validation = loader.split.val
+validation = loader.split.test
 print(train[0][1])
 
 # Test-6 - Convert to gpu ready
 from ocr.pytorch.util import gpu_format
 from parser.lookup import codebook
-lookup_filename = "/OCRData2/ocr/retrain/src/parameters/lookups/Malayalam.txt"
+# lookup_filename = "/OCRData2/ocr/retrain/src/parameters/lookups/Malayalam.txt"
+lookup_filename = 'Sanskrit.txt'
 lmap, ilmap = codebook(lookup_filename)
-print(gpu_format(lmap)(train[0]))
-
+# print(gpu_format(lmap)(train[0]))
+# pdb.set_trace()
 
 # Test-8 - Convert to GPU
 convert_gpu = lambda x: list(map(gpu_format(lmap), x))
@@ -75,7 +78,8 @@ except FileNotFoundError:
 #train_set = train_set[:300]
 #validation_set = validation_set[:30]
 engine = Engine(**kwargs)
-while not satisfactory:
-    val_err, train_err = engine.train(train_set, validation_set, debug=True)
-    kwargs = engine.export()
-    torch.save(kwargs, open(savepath, "wb+"))
+# while not satisfactory:
+print('Training')
+val_err, train_err = engine.train(train_set, debug=True)
+kwargs = engine.export()
+torch.save(kwargs, open(savepath, "wb+"))
